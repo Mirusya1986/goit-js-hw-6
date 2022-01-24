c1ad642bdad8879bdd900d56d4ae528e47f6e837
Mirusya1986 <k.mirochka@gmail.com>
2022-01-24 23:11:11 +0200

    task2

--- js/task-01.js
+++ js/task-01.js
@@ -3,3 +3,7 @@ const items = document.querySelectorAll ('.item');
 const numberOfCategories = items.length;
 console.log(`Number of categories: ${numberOfCategories}`);
 
+for (let i=0; i<items.length; i+=1){
+console.log(`Category: ${items[i].children[0].innerHTML}`)
+console.log(`Elements: ${items[i].children[1].children.length}`)
+}
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ul = document.createElement ('ul');
+const lis=ingredients.map((ingredient) =>{
+  const li = document.createElement ('li');
+  li.textContent = ingredient;
+
+return li;
+});
+ul.append (...lis);
+
+document.body.appendChild(ul);
