dfe26023b79fb84efc0e6f390712882d1a66dec8
Mirusya1986 <k.mirochka@gmail.com>
2022-02-07 00:38:45 +0200

    tasks_8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
 
+const submitForm = document.querySelector(`.login-form`);
+
+console.dir(submitForm);
+
+submitForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e){
+    e.preventDefault();
+
+    const submitFormElements = e.currentTarget.elements;
+    const mail = submitFormElements.email.value;
+    const password = submitFormElements.password.value;
+
+
+    if (mail ===''|| password===''){
+        return alert (`Attention! All fields must be filled!`)
+
+    }
+
+
+    const formData = {
+        mail,
+        password,
+
+    };
+
+    console.log(formData);
+    submitForm.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const spanColor = document.querySelector(`.color`);
+const buttonChangeColor = document.querySelector(`.change-color`)
+
+buttonChangeColor.addEventListener(`click`, onButtonClick);
+
+
+function onButtonClick (){
+  const color = getRandomHexColor();
+
+  document.body.style.backgroundColor = color;
+  spanColor.textContent = color;
+  console.log(spanColor.textContent); 
+}
