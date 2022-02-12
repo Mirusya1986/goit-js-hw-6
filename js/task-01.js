cf85f9b57ff50099c318663f0bf6ba61cc5eaca9
Mirusya1986 <k.mirochka@gmail.com>
2022-02-13 01:07:31 +0200

    revised

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,23 @@
 const items = document.querySelectorAll ('.item');
 
+
+
+
 const numberOfCategories = items.length;
 console.log(`Number of categories: ${numberOfCategories}`);
 
-for (let i=0; i<items.length; i+=1){
-console.log(`Category: ${items[i].children[0].innerHTML}`)
-console.log(`Elements: ${items[i].children[1].children.length}`)
-}
+// for (let i=0; i<items.length; i+=1){
+// console.log(`Category: ${items[i].children[0].innerHTML}`)
+// console.log(`Elements: ${items[i].children[1].children.length}`)
+// }
+
+
+
+
+items.forEach((item) =>{
+    const heading = item.querySelector("h2");
+    const categories = item.querySelectorAll("li");
+    
+    console.log("Category:", heading.textContent);
+    console.log("Elements:", categories.length);
+});
--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const inputEl = document.querySelector('#font-size-control');
 const textResult = document.querySelector('#text');
 
+textResult.style.fontSize=`${inputEl.value}px`;
+
 
 const onChahge = (e) => 
     textResult.style.fontSize=`${e.currentTarget.value}px`;
